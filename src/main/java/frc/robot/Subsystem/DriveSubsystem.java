a4f77dbe9876f1d802ee75fa8821af6d809c1573
Minesoil <justintuan1201@gmail.com>
2024-11-19 18:15:24 +0800

    got rid of unsued library

--- src/main/java/frc/robot/Subsystem/DriveSubsystem.java
+++ src/main/java/frc/robot/Subsystem/DriveSubsystem.java
@@ -5,7 +5,6 @@ import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.TankPID;
 
